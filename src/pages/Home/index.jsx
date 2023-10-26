932ff6b33acc692143ec5d739c8a52467509be95
jesus bravo <jd_bravo24@hotmail.com>
2023-10-26 13:57:40 -0500

    booking

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,13 @@
 import { Navbar } from '../../components/Navbar';
 import { Header } from '../../components/Header';
+import { Booking } from '../../components/Booking';
 
 export const Home = () => {
   return (
     <div>
       <Navbar />
       <Header />
+      <Booking />
     </div>
   )
 };
